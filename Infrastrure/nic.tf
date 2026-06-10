3587b389be44e6b2244a355bed172accb15fdab8
Vignesh Venkatachalam <vignesh.venkatachalam@cloud4c.com>
2026-06-10 22:40:39 +0530

    added nic & public-ip file

---
+++ Infrastrure/nic.tf
@@ -0,0 +1,17 @@
+resource "azurerm_network_interface" "app_nic" {
+
+  name                = "app-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+
+    name = "internal"
+
+    subnet_id = azurerm_subnet.app_subnet.id
+
+    private_ip_address_allocation = "Dynamic"
+
+    public_ip_address_id = azurerm_public_ip.vm_pip.id
+  }
+}
